a74044a5ad8a695889a60605189640c88805dd43
felipegresele <felipehgresele@gmail.com>
2026-04-22 14:42:39 -0300

    fix: ajustes index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,26 @@
 import axios from "axios"
 import { useAuthStore } from "./auth-store/authStore"
 
- 
 export const api = axios.create({
   baseURL: 'https://smart-tasks-backend.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
- 
+
 api.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
- 
+
 api.interceptors.response.use(
   (res) => res,
   (error) => {
-    if (error.response?.status === 401) {
+    // Só redireciona para login se NÃO for uma rota de autenticação
+    const isAuthRoute = error.config?.url?.includes('/auth/')
+    if (error.response?.status === 401 && !isAuthRoute) {
       useAuthStore.getState().logout()
       window.location.href = '/login'
     }
     return Promise.reject(error)
   }
 )
- 
